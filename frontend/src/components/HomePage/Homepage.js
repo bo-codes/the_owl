ec426c998b3639d7f4c4dab0c3d21562cd56fb27
elias <99310710+bo-codes@users.noreply.github.com>
2023-01-07 15:37:03 -0800

    deleted unnecessary files from react-templalte

--- frontend/src/components/HomePage/Homepage.js
+++ frontend/src/components/HomePage/Homepage.js
@@ -6,25 +6,7 @@ import "./HomePage.css";
 function HomePage() {
   return (
     <div id="page-container">
-      <div id="page-first-half">
-        <div id="post-detail-container">
-          <div id="post-title">Post title</div>
-          <div id="post-content">
-            {/* if photo */}
-            <div id="post-photo"></div>
-            <div id="post-description">description</div>
-          </div>
-        </div>
-      </div>
-      <div id="page-second-half">
-        <div id="main-title-container">
-          <h1 id="main-title-text">ELI'S THOUGHTS</h1>
-        </div>
-        <div id="post-list">
-          {/* map posts (for each post) => */}
-          <div id="post-preview">{/* jsut the title and date */}</div>
-        </div>
-      </div>
+      <h1>hello</h1>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,8 @@ import App from "./App";
 import "./index.css";
 
 
-
+// the function below from react-dom will execute and cause the browser to render all of our jsx (compiled and organzed in our App.js function component)
+// it will be rendered in the index.html file at the div where id='root'
 
 ReactDOM.render(
   <React.StrictMode>
