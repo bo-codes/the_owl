3543a9f30f2969bb4461c225f9dfe8b79d7d7c13
elias <99310710+bo-codes@users.noreply.github.com>
2023-01-08 20:38:48 -0800

    adding notes to react app building process

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, NavLink, Route, Switch } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
