c66fc97ee5bdfcdd174b470b0c1bed66bc326ae6
elias <99310710+bo-codes@users.noreply.github.com>
2023-02-24 09:23:12 -0800

    installed redux and redux-toolkit and set files up to implement redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Provider } from "./Context/books";
+// import { Provider } from "./Context/books";
 // import BooksContext from "./Context/books";
 import "./index.css";
-
+import { store } from "./Store";
+import { Provider } from "react-redux";
 
 // the function below from react-dom will execute and cause the browser to render all of our jsx (compiled and organzed in our App.js function component)
 // it will be rendered in the index.html file at the div where id='root'
 // ReactDOM.render(
 //   // <React.StrictMode>
-
 
 //   // We are wrapping our entire application with the BooksContext that we built and imported from our context folder
 //   // so that our entire application has access to the values stored in this context
@@ -21,7 +21,6 @@ import "./index.css";
 //   //   <App />
 //   // </BooksContext.Provider>,
 
-
 //   // THIS VERSION OF THE CODE IS FOR US TO PASS IN OUR PROVIDER COMPONENT WE CREATED IN TEH CONTEXT FILE AND WRAP THE APP WITH IT INSTEAD OF THE CONTEXT.PROVIDER
 //   // US BUILDING THE PROVIDER IN THE AFFOREMENTIONED CONTEXT FILE ALLOWS US TO INCLUDE A SLICE OF STATE IN IT AND PASS THAT SLICE OF STATE (BOTH VARIABLE AND FUNCTION TO UPDATE THE VARIABLE)
 //   // INTO OTHER COMPONENTS
@@ -29,9 +28,7 @@ import "./index.css";
 //     <App />
 //   </Provider>,
 
-
 //   // REFER TO NAVIGATION.JS TO SEE HOW CONTEXT IS IMPORTED AND IMPLEMENTED
-
 
 //   // </React.StrictMode>,
 
@@ -39,15 +36,14 @@ import "./index.css";
 //   document.getElementById("root")
 // );
 
-
 // ----------- CAN ALSO BE WRITTEN AS ----------- //
 
-const el = document.getElementById('root')
+const el = document.getElementById("root");
 
 const root = ReactDOM.createRoot(el);
 
 root.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>
-)
+);
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,23 +1,56 @@
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import sessionReducer from "./session";
+import {createSlice, configureStore} from '@reduxjs/toolkit'
 
-const rootReducer = combineReducers({
-    session: sessionReducer,
-});
 
-let enhancer;
+const postsSlice = createSlice({
+    name: 'post',
+    initialState: {},
+    reducers: {
+        addPost(state, action) {
+            // key into the action to get our payload, and get our post we are passing through there
+            state[action.payload.post.id] = action.payload.post
+        },
+        removePost(state, action) {
+            delete state[action.postId]
+        }
+    }
+})
 
-if (process.env.NODE_ENV === "production") {
-    enhancer = applyMiddleware(thunk);
-} else {
-    const logger = require("redux-logger").default;
-    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-    enhancer = composeEnhancers(applyMiddleware(thunk, logger));
-}
+const store = configureStore({
+    reducer: {
+        posts: postsSlice.reducer
+    }
+})
 
-const configureStore = preloadedState => {
-    return createStore(rootReducer, preloadedState, enhancer);
-};
+// store.dispatch({
+//     type: 'post/addPost',
+//     payload: 'New Post!!!'
+// })
 
-export default configureStore;
+// store.dispatch(postsSlice.actions.addPost({id: 1, post: {content: 'New Post', createdAt: 'today', userId: '1'}}))
+
+
+// import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+// import thunk from "redux-thunk";
+// import sessionReducer from "./session";
+
+// const rootReducer = combineReducers({
+//     session: sessionReducer,
+// });
+
+// let enhancer;
+
+// if (process.env.NODE_ENV === "production") {
+//     enhancer = applyMiddleware(thunk);
+// } else {
+//     const logger = require("redux-logger").default;
+//     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//     enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+// }
+
+// const configureStore = preloadedState => {
+//     return createStore(rootReducer, preloadedState, enhancer);
+// };
+
+// export default configureStore;
+
+export { store };
